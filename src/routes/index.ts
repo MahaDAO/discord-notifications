8a25e06641370078bdf4967a92aa4a64aee6eeb0
Aniketh Panchidi <ani.p1995@gmail.com>
2023-06-27 16:35:06 +0530

    minor change

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getMahalock } from "../controller/mahaLock";
 
 const router = Router();
-router.get("/", (req, res) => console.log("server is running"));
+router.get("/", (req, res) => res.send("server is running"));
 router.post("/mahalock-webhook", (req, res) => getMahalock(req, res));
 
 export default router;
